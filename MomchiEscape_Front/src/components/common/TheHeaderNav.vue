83327b94fba343a579e6b56296b3d9a2a55252e5
haelim12 <sunny9612@naver.com>
2023-11-16 11:42:02 +0900

    [Change] Header Color

--- MomchiEscape_Front/src/components/common/TheHeaderNav.vue
+++ MomchiEscape_Front/src/components/common/TheHeaderNav.vue
@@ -1,38 +1,36 @@
 <template>
-    <div id="container">
-        <header>
-            <nav>
-                <RouterLink to="/">Home</RouterLink> |
-                <RouterLink to="/youtube">Youtube</RouterLink> |
-                <RouterLink :to="{ name: 'boardList' }">BoardList</RouterLink> |
-                <RouterLink :to="{ name: 'boardCreate' }">BoardCreate</RouterLink> |
-                <RouterLink :to="{name: 'kakao'}">Kakao</RouterLink> |
-                <RouterLink :to="{name: 'tmdb'}">TMDB</RouterLink> 
-            </nav>
-        </header>
-    </div>
+  <div id="container">
+    <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink> |
+        <RouterLink to="/youtube">Youtube</RouterLink> |
+        <RouterLink :to="{ name: 'boardList' }">BoardList</RouterLink> |
+        <RouterLink :to="{ name: 'boardCreate' }">BoardCreate</RouterLink> |
+        <RouterLink :to="{ name: 'kakao' }">Kakao</RouterLink> |
+        <RouterLink :to="{ name: 'tmdb' }">TMDB</RouterLink>
+      </nav>
+    </header>
+  </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style  scoped>
+<style scoped>
 #container {
-    text-align: center;
+  text-align: center;
 }
 
 nav {
-    padding: 30px
+  padding: 30px;
 }
 
 nav a {
-    font-weight: bold;
-    text-decoration: none;
-    color: black;
+  font-weight: bold;
+  text-decoration: none;
+  color: #404141;
 }
 
 nav a.router-link-exact-active {
-    color: #42b983
+  color: #eac3c9;
 }
 </style>

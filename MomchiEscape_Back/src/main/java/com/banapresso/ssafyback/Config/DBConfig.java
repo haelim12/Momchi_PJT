9f40a994f44813a02995098b2b16cc0e73fcf57d
AhyngK <canon1107@naver.com>
2023-11-16 11:27:47 +0900

    Project Files

---
+++ MomchiEscape_Back/src/main/java/com/banapresso/ssafyback/Config/DBConfig.java
@@ -0,0 +1,9 @@
+package com.banapresso.ssafyback.Config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@MapperScan("com.banapresso.ssafyback.Post")
+public class DBConfig {
+}
---
+++ MomchiEscape_Back/src/main/java/com/banapresso/ssafyback/Controller/UserController.java
@@ -0,0 +1,7 @@
+package com.banapresso.ssafyback.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public  UserController {
+//}

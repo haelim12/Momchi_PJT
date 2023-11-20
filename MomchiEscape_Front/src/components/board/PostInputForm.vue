4272fde93d08e90146b5f2cb06d146b01a9f3f0a
haelim12 <sunny9612@naver.com>
2023-11-20 09:42:53 +0900

    [Fixed] BoardCreate

--- MomchiEscape_Front/src/components/board/PostInputForm.vue
+++ MomchiEscape_Front/src/components/board/PostInputForm.vue
@@ -1,37 +1,40 @@
 <template>
   <div class="input-container">
     <div class="name">{{ props.name }}</div>
+    <textarea
+      v-if="props.type === 'textarea'"
+      v-model="inputValue"
+      class="text-input-textarea"
+      :placeholder="props.placeholder"
+      @input="$emit('send-data', inputValue)"
+    ></textarea>
     <input
+      v-else
       v-model="inputValue"
       :type="props.type"
       class="text-input"
       :placeholder="props.placeholder"
-      @change="$emit('send-data', inputValue)"
+      @input="$emit('send-data', inputValue)"
     />
   </div>
 </template>
 
 <script setup>
-import { ref, defineProps, defineEmits, onMounted } from "vue";
-import { usePostStore } from "@/stores/postStore";
+import { ref, defineProps, defineEmits } from "vue";
 
-const postStore = usePostStore();
 const props = defineProps(["type", "name", "placeholder"]);
 const inputValue = ref("");
 const emit = defineEmits(["send-data"]);
-
-onMounted(() => {
-  // console.log(props.name);
-});
 </script>
 
 <style scoped>
 .input-container {
   width: 400px;
-  height: 100px;
+  /* height: ; */
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding-bottom: 30px;
 }
 .name {
   width: 350px;
@@ -49,10 +52,22 @@ onMounted(() => {
   border: solid #999999 1px;
   border-radius: 5px;
 }
+
+.text-input-textarea {
+  width: 340px;
+  height: 200px;
+  padding-left: 7px;
+  padding-top: 10px;
+  font-size: 12px;
+  font: #999999;
+  border: solid #999999 1px;
+  border-radius: 5px;
+  resize: none;
+}
 .message {
   width: 350px;
   text-align: left;
-  margin-left: 5px;
+  /* margin-left: 5px; */
   font-size: 12px;
   margin-top: 7px;
   margin-bottom: 5px;
